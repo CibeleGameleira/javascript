a121055f3bd89862b0c12c6d1d1fb6c670adf989
Cibele Mendes Gameleira <cibelewbcmendes@gmail.com>
2023-10-04 02:35:53 -0300

    estudo variaveis

---
+++ conceitos-basicos/variaveis/var.js
@@ -0,0 +1,22 @@
+/**
+ * O que é uma variável?
+ * Um lugar onde armazena dados.  
+ * obs: dados = menor parte de uma informação 
+ * De forma genérica, como construir uma variável
+ *  É preciso o escopo e o identificador.
+ * Para fazer uma atribuição, você pega o identificador, o sinal de atribuição e o dado.
+ * Para declarar uma variável no javascript:
+ * */
+
+
+// Declarando a variavel animal, e atribuindo o dado cachorro
+var animal = 'cachorro';
+
+console.log(animal);
+
+var animal = "shih tzu";
+
+console.log(animal);
+
+// Declarando a variavel quantidade de chocolates e atribuindo o valor 16
+var quantidadeDeChocolates = 16;
