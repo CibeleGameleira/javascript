afd57026cf396d60c594275a135f287cf9ccdebb
CibeleGameleira <cibelewbcmendes@gmail.com>
2023-10-15 17:42:31 -0300

    aprendendo a usar variaveis em operacoes matematicas e exibi-las no terminal

---
+++ conceitos-basicos/variaveis/usando-variaveis/calculadora.js
@@ -0,0 +1,52 @@
+// Adição +
+// Adicionar um valor em um número já existente; somar um valor no outro.
+
+// Entrada 
+const meninosNaSala = 18; // Entrada
+const meninasNaSala = 21; // Entrada
+const alunosNaSala = meninosNaSala + meninasNaSala; // Processamento 
+
+//Saída 
+console.log(`a operacao ${meninosNaSala} + ${meninasNaSala} = ` + alunosNaSala); // Saída
+
+console.log('a operação ' + meninosNaSala + ' + ' + meninasNaSala + ' = ' + alunosNaSala);  //  Saída
+
+console.log ("a operação " + meninosNaSala + " + " + meninasNaSala + " = " + alunosNaSala); // Saída
+
+
+
+// Subtração -
+// Remover um valor em um número já existente; tirar um valor do outro.
+
+
+const sapatosComprados = 358; // Entrada
+const sapatosVendidos = 67; // Entrada 
+const sapatosVitrine = sapatosComprados - sapatosVendidos; //Processamento
+
+console.log('a operação ' + sapatosComprados  + ' - ' +  sapatosVendidos + ' = ' + sapatosVitrine); // Saída
+
+console.log(sapatosVitrine);
+
+
+
+// Multiplicação *
+// Multiplicar um valor em um número já existente; fazer um valor aumentar.
+
+const pessoasCinema = 200; // Entrada
+const valorIngresso = 40.50; // Etrada
+const lucroCinema = pessoasCinema * valorIngresso; // Processamento
+
+console.log(`a operação ${pessoasCinema} * ${valorIngresso} = ${lucroCinema} `); // Saída
+
+console.log(lucroCinema);
+
+// Divisão /
+// Repartir um valor em duas ou várias partes; dividir um número.
+
+const criancas = 10; // Entrada
+const quantidadePirulitos = 30; // Entrada
+const pirulitosCrianca = criancas / quantidadePirulitos; // Processamento
+
+console.log(`a operação ${criancas} / ${quantidadePirulitos} = ${pirulitosCrianca} `); // Saída
+
+console.log(pirulitosCrianca);
