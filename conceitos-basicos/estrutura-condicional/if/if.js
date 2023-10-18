185e63350b80616263b64e044078b14b58e2b11f
gabriel.gsantos <gabriel.gsantos@totvs.com.br>
2023-10-18 00:54:57 -0300

    estrutura de condicional if

---
+++ conceitos-basicos/estrutura-condicional/if/if.js
@@ -0,0 +1,27 @@
+const votosLula = 59563912;
+const votosBolsonaro = 57675427;
+
+if (votosLula > votosBolsonaro) {
+    console.log('o amor venceu');
+};
+
+if (votosLula < votosBolsonaro) {
+    console.log("mito mito mito");
+};
+
+if (votosLula == votosBolsonaro) {
+    console.log(`o brasil venceu`);
+};
+
+const temperaturaCorporal = 37;
+const temperaturaFebre = 38;
+const temperaturaNormal = 37;
+
+if (temperaturaCorporal >= temperaturaFebre) {
+    console.log('estou tendo falencia multipla de orgaos');
+};
+
+if (temperaturaCorporal <= temperaturaNormal) {
+    console.log (`ta tranquilo ta favoravel`);
+};
+
